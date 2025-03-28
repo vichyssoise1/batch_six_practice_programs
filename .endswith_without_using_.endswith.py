cc45a5dc6b7e6eb01973178459e46bdf750f2128
vichyssoise <carlosbondocdizon@gmail.com>
2025-03-28 23:06:21 +0800

    added .endswith function without using .endswith

--- .endswith_without_using_.endswith.py
+++ .endswith_without_using_.endswith.py
@@ -1,3 +1,8 @@
 #user input
 text = input("Enter a string: ")
 suffix = input("Enter the edning to check: ")
+
+#Check if the string ends with the given suffix
+ends_with = text[-len(suffix):] == suffix
+
+print("Does the string end with the given suffix?", ends_with)
