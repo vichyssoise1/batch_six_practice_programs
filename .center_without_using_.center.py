aabc839f820beac4326b6780a5203f7f712dc237
vichyssoise <carlosbondocdizon@gmail.com>
2025-03-28 23:17:34 +0800

    added .center function without using .center

--- .center_without_using_.center.py
+++ .center_without_using_.center.py
@@ -2,4 +2,8 @@
 text = input("Enter a string: ")
 width = int(input("Enter the total width: "))
 
-print(text)
+if len (text) < width:
+    spaces = (width - len(text)) // 2
+    text = " " * spaces + text + " " * (width - len(text) - spaces)
+
+print("The centered string is: ", text)
