54ed699dcde100f65d23f309877ef0bc4b8821c4
vichyssoise <carlosbondocdizon@gmail.com>
2025-03-28 23:34:45 +0800

    added .title function without using .title

--- .title_without_using_.title.py
+++ .title_without_using_.title.py
@@ -1,4 +1,7 @@
 #User input
 text = input("Enter a string: ")
 
-print("The string is: ", text)
+words = text.split()  # Split the string into words
+capitalized_words = [word[0].upper() + word[1:].lower() if word else "" for word in words] # Capitalize each word
+result = " ".join(capitalized_words)  # Join the words back into a string
+print("Title cased string:", result)  # Print the result
