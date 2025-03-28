87cac08e5800ae709bed383436729d89398736ff
vichyssoise <carlosbondocdizon@gmail.com>
2025-03-28 23:21:09 +0800

    added swapcase funtion without using swapcase

--- .swapcase_without_using_.swapcase.py
+++ .swapcase_without_using_.swapcase.py
@@ -1,4 +1,7 @@
 #User input
 text = input("Enter a string: ")
 
+# Swap case without using swapcase() method
+text = ''.join(char.lower() if char.isupper() else char.upper() for char in text)
+
 print("The string is: ", text)
