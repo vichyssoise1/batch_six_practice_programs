29ec748d423188c68bfcc977e9a6d8046c90e47d
vichyssoise <carlosbondocdizon@gmail.com>
2025-03-28 22:38:24 +0800

    remove leading spaces without using lstrip function

--- .lstrip_without.lstrip_function.py
+++ .lstrip_without.lstrip_function.py
@@ -1,3 +1,4 @@
 #User Input
 full_name = input("Enter your full name: ")
+full_name = " ".join(full_name.split()) #remove leading spaces without using lstrip function
 print("Your name without leading spaces is: ", full_name)
