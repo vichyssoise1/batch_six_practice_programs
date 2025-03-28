3f6a3acabac792ec442eb3c4139902a72141d593
vichyssoise <carlosbondocdizon@gmail.com>
2025-03-28 23:11:01 +0800

    added .ljust function without using .ljust function

--- .ljust_without_using_.ljust.py
+++ .ljust_without_using_.ljust.py
@@ -2,5 +2,7 @@
 text = input("Enter string: ")
 width = int(input("Enter the total width: "))
 
+if len(text) < width:
+    text += " " * (width - len(text))  # Add spaces to the right to fill the width
 
 print("The string is: ", text)
