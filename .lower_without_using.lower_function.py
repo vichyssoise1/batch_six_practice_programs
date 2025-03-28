df7601b46def2ea5b036c752e25c667a44ccf8a9
vichyssoise <carlosbondocdizon@gmail.com>
2025-03-28 22:55:13 +0800

    made text lowercase without using .lower function

--- .lower_without_using.lower_function.py
+++ .lower_without_using.lower_function.py
@@ -1,3 +1,7 @@
 #user input
 text = input("Enter a string: ")
-print("Lowercase string:", text)
+
+# Convert uppercase letters (A-Z) to lowercase manually
+lower_text = "".join(chr(ord(char) + 32) if 'A' <= char <= 'Z' else char for char in text)
+
+print("Lowercase string:", lower_text)
