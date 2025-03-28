3a35856945b4cccc5a037bbc33e0c59f770659e1
vichyssoise <carlosbondocdizon@gmail.com>
2025-03-28 22:50:09 +0800

    added a function to remove the prefix without using removeprefix

--- removeprefix_without_removeprefix_function.py
+++ removeprefix_without_removeprefix_function.py
@@ -2,5 +2,9 @@
 text = input("Enter a string: ")
 prefix = input("Enter the prefix to remove: ")
 
+#funtion to remove the prefix
+if text.startswith(prefix):
+    text = text[len(prefix):] # remove the prefix
+
 #check if it works
-print("Original string: ", text)
+print("Starting without the prefix: ", text)
