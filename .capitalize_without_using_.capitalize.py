26a75fc59d5fde7b053904440a0a92c938570f6e
vichyssoise <carlosbondocdizon@gmail.com>
2025-03-28 23:28:40 +0800

    added .capitalize function without using .capitalize

--- .capitalize_without_using_.capitalize.py
+++ .capitalize_without_using_.capitalize.py
@@ -1,4 +1,10 @@
 #User input
 text = input("Enter a string: ")
 
-print("The string is: ", text)
+if text:
+    # Capitalize the first letter of the string
+    result = text[0].upper() + text[1:].lower()
+else:
+    result = text 
+
+print("The string is: ", result)
