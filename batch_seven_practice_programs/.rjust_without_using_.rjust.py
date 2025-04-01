caee5990d9c0275d7c84e5ec28fde0c9b3ba56e3
vichyssoise <carlosbondocdizon@gmail.com>
2025-04-01 22:03:36 +0800

    added function to right justify without using .rjust

--- batch_seven_practice_programs/.rjust_without_using_.rjust.py
+++ batch_seven_practice_programs/.rjust_without_using_.rjust.py
@@ -1,9 +1,10 @@
-# Create uswe inpur
+# Create user inpur
 text = input("Enter a string: ")
 width = int(input("Enter the total width: "))
 
+# Function to right justify a string without using rjust()
+# Add space characters at the beginning of the string to complete the total width
+padded_text = ' ' * (width - len(text)) + text if len(text) < width else text
 
-
-# Check if the input works
-print("Input string: ", text)
-print("Input width: ", width)
+# Print the result
+print("Right-justified string:", padded_text)
