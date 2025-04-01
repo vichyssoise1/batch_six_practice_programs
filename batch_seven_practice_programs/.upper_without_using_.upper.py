3dddbb9a01485b47c2450bcc0f9b2aad8e358898
vichyssoise <carlosbondocdizon@gmail.com>
2025-04-01 21:38:32 +0800

    convert to uppercase without using the .upper function

--- batch_seven_practice_programs/.upper_without_using_.upper.py
+++ batch_seven_practice_programs/.upper_without_using_.upper.py
@@ -1,5 +1,8 @@
 # Add user input
 text = input("Enter string: ")
 
+# Convert to uppercase without using the upper() method
+uppercase_text = ''.join(chr(ord(char) - 32) if 'a' <= char <= 'z' else char for char in text)
 
-print("You entered: ", text)
+# Print the result
+print("Uppercased string: ", uppercase_text)
