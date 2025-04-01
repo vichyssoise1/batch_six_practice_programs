2e4671c413e2a8573eeafeed1a5de7de4d3e057a
vichyssoise <carlosbondocdizon@gmail.com>
2025-04-01 21:43:09 +0800

    Check if its lower without using .islower function

--- batch_seven_practice_programs/.islower_without_using_.islower.py
+++ batch_seven_practice_programs/.islower_without_using_.islower.py
@@ -1,5 +1,8 @@
 # Add user input to collect the string
 text = input("Enter a string: ")
 
+# Check if the string is lowercase without using .islower()
+all_lower = all('a' <= char <= 'z' for char in text if char.isalpha())
+
 # Check if it works
-print("You entered:", text)
+print("The String is all loercase: ", all_lower)
