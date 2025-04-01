f39383b7d44c0b59dfd181a8cf2c31c672176773
vichyssoise <carlosbondocdizon@gmail.com>
2025-04-01 22:01:48 +0800

    added user input

---
+++ batch_seven_practice_programs/.rjust_without_using_.rjust.py
@@ -0,0 +1,9 @@
+# Create uswe inpur
+text = input("Enter a string: ")
+width = int(input("Enter the total width: "))
+
+
+
+# Check if the input works
+print("Input string: ", text)
+print("Input width: ", width)
