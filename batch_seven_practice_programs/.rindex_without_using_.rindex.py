a3304263ed3a1418629251589dd68d081ad4ceaa
vichyssoise <carlosbondocdizon@gmail.com>
2025-04-01 22:38:49 +0800

    Added .rindex function using .rfind to replicate the function

--- batch_seven_practice_programs/.rindex_without_using_.rindex.py
+++ batch_seven_practice_programs/.rindex_without_using_.rindex.py
@@ -1,7 +1,9 @@
 # Create user input 
-text = input("Enter text: ")
+text = input("Enter string: ")
 substring = input("Enter the substring to search for: ")
 
-# Check if it works
-print(text)
-print(substring)
+# Find the last occurrence of the substring manually
+position = text.rfind(substring)
+
+# Print the result
+print(f"The last occurrence of '{substring}' is at index {position}" if position != -1 else "Substring not found.")
