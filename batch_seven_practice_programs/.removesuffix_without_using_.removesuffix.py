97400f72d8edd714ae891ce603c44a0612f71bcd
vichyssoise <carlosbondocdizon@gmail.com>
2025-04-01 21:33:29 +0800

    added .removesuffix function wouthout using .removesuffix

--- batch_seven_practice_programs/.removesuffix_without_using_.removesuffix.py
+++ batch_seven_practice_programs/.removesuffix_without_using_.removesuffix.py
@@ -2,5 +2,9 @@
 text = input("Enter a string: ")
 suffix = input("Enter a suffix: ")
 
+# Function to check if the string ends with the given suffix
+if text.endswith(suffix):
+    text = text[:-len(suffix)] # Remove the suffix from the string
+
 # Print the result of the function
-print("Your string ends with the suffix: ", text)
+print("Your string after removing the suffix: ", text)
