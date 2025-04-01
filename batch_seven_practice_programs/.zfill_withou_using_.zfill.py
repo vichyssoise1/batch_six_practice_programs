012698e214d5714c39fa2637435a682d2d059124
vichyssoise <carlosbondocdizon@gmail.com>
2025-04-01 22:09:49 +0800

    Added zeroes with the specified width without using .zfill

--- batch_seven_practice_programs/.zfill_withou_using_.zfill.py
+++ batch_seven_practice_programs/.zfill_withou_using_.zfill.py
@@ -2,6 +2,8 @@
 text = input("Enter text: ")
 width = int(input("Enter total width: "))
 
-# Check if it works
-print("Text: ", text)
-print("Width: ", width)
+# Function to add zero at the beginning of the string to complete the total width
+padded_text = '0' * (width - len(text)) + text if len(text) < width else text 
+
+# print the result
+print("Zero-padded text:", padded_text)
